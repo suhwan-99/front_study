98f46521c145a58d0a0bcb13213bbc2de8d32086
unknown <kaghop9@gamil.com>
2025-07-10 21:06:12 +0900

    0710 숙제 완

--- 0710/8.다크모드만들기/script.js
+++ 0710/8.다크모드만들기/script.js
@@ -3,5 +3,14 @@ const body = document.body;
 btn.addEventListener('click', function(e){
   body.style.color = 'white';
   body.style.background = 'black';
-
+  document.querySelector('.r-btn').style.display = 'block';
+  document.querySelector('.btn').style.display = 'none';
 });
+
+const btn2 = document.querySelector('.r-btn');
+btn2.addEventListener('click', function(e){
+  body.style.color = 'black';
+  body.style.background = 'white';
+  document.querySelector('.btn').style.display = 'block';
+  document.querySelector('.r-btn').style.display = 'none';
+})
